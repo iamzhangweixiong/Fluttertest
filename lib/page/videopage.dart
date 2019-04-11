ee9d1d34ad00e7db38fcf314f5067a5936c4aa23
zhangwx <zhangwxgo@gmail.com>
2019-04-11 11:02:49 +0800

    【commit】video player 又可以用了，神奇

--- lib/page/videopage.dart
+++ lib/page/videopage.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-//import 'package:video_player/video_player.dart';
+import 'package:video_player/video_player.dart';
 
 class VideoPage extends StatefulWidget {
   VideoPage({Key key, this.sourceId}) : super(key: key);
@@ -11,25 +11,25 @@ class VideoPage extends StatefulWidget {
 }
 
 class VideoPageState extends State<VideoPage> {
-//  VideoPlayerController videoController;
+  VideoPlayerController videoController;
   bool _isPlaying = false;
   String url = 'https://dl-cmshow.cmcm.com/video/upload_3f9e7b015fb5aec69c11dd7d47b641a9.mp4';
 
   @override
   void initState() {
     super.initState();
-//    videoController = VideoPlayerController.network(url)
-//      ..addListener(() {
-//        final bool isPlaying = videoController.value.isPlaying;
-//        print(isPlaying);
-//        if (isPlaying != _isPlaying) {
-//          setState(() { _isPlaying = isPlaying; });
-//        }
-//      })
-//      ..initialize().then((_) {
-//        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
-//        setState(() {});
-//      });
+    videoController = VideoPlayerController.network(url)
+      ..addListener(() {
+        final bool isPlaying = videoController.value.isPlaying;
+        print(isPlaying);
+        if (isPlaying != _isPlaying) {
+          setState(() { _isPlaying = isPlaying; });
+        }
+      })
+      ..initialize().then((_) {
+        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
+        setState(() {});
+      });
   }
 
   @override
@@ -37,26 +37,26 @@ class VideoPageState extends State<VideoPage> {
     return MaterialApp(
       title: 'Video Demo',
       home: Scaffold(
-//        body: Center(
-//          child: videoController.value.initialized
-//              ? AspectRatio(
-//                  aspectRatio: videoController.value.aspectRatio,
-//                  child: VideoPlayer(videoController),
-//                )
-//              : Container(),
-//        ),
-//        floatingActionButton: FloatingActionButton(
-//          onPressed: () {
-//            setState(() {
-//              videoController.value.isPlaying
-//                  ? videoController.pause()
-//                  : videoController.play();
-//            });
-//          },
-//          child: Icon(
-//            videoController.value.isPlaying ? Icons.pause : Icons.play_arrow,
-//          ),
-//        ),
+        body: Center(
+          child: videoController.value.initialized
+              ? AspectRatio(
+                  aspectRatio: videoController.value.aspectRatio,
+                  child: VideoPlayer(videoController),
+                )
+              : Container(),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              videoController.value.isPlaying
+                  ? videoController.pause()
+                  : videoController.play();
+            });
+          },
+          child: Icon(
+            videoController.value.isPlaying ? Icons.pause : Icons.play_arrow,
+          ),
+        ),
       ),
     );
   }
@@ -64,6 +64,6 @@ class VideoPageState extends State<VideoPage> {
   @override
   void dispose() {
     super.dispose();
-//    videoController.dispose();
+    videoController.dispose();
   }
 }
